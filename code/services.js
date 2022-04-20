7140430f8cba270f2be001f0a3ccab4dbbc6ae2c
daridev <hernandezdarifrancisco@gmail.com>
2022-04-19 21:54:17 -0500

    services, hostfix show articles bug

--- code/services.js
+++ code/services.js
@@ -4,16 +4,26 @@ var current_article = 1
 
 // List of articles
 var buttons = []
+var articles = []
 
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s*1000));
 }
 
 // Get all buttons
-for (let button_num = 0; button_num<=6; button_num++) {
-    let selector_button = "#btn-service-" + button_num
+for (let num = 0; num<=6; num++) {
+    
+    // Get button
+    let selector_button = "#btn-service-" + num
     let button = document.querySelector (selector_button)
+
+    // Get article
+    let selector_article = "#service-" + num
+    let article = document.querySelector (selector_article)
+
+    // Save articles and buttons
     buttons.push (button)
+    articles.push (article)
 }
 
 // Detect buttons clicks
@@ -46,17 +56,21 @@ buttons_wrapper.addEventListener ("click", async function (event) {
     // Activate new button
     target_button.classList.add ("active")
     
-
-    // Get the last article
-    let selector_last_article = '#service-' + current_article
-    let article_last = document.querySelector (selector_last_article)
-
-    // Hide the last article
-    article_last.classList.add ("transparent")
+    // Loop for each article
+    for (let article_last of articles) {
+        if (article_last) {
+            // Make article transparent
+            article_last.classList.add ("transparent")
+        } 
+    }
     await sleep(0.25)
-    article_last.classList.add ("hide")
-
-
+    for (let article_last of articles) {
+        if (article_last) {
+            // Hide article
+            article_last.classList.add ("hide")
+        }
+    }
+    
     // Get the new article
     let selector_new_article = '#service-' + num
     let new_article = document.querySelector (selector_new_article)
